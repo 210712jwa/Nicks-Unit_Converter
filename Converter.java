a3bd6c089b3968f2af24419bf823a0a860f80438
nahailey <87378846+nahailey@users.noreply.github.com>
2021-07-19 10:47:38 -0400

    Update Converter.java

--- Converter.java
+++ Converter.java
@@ -158,7 +158,7 @@ public class Converter {
 			}
 			System.out.println("Would you like to choose another Conversion? (Y/N) ");
 			
-			tryAnother = sc.nextLine();
+		
 			tryAnother = sc.nextLine();
 
 			if (tryAnother.equals("N") || tryAnother.equals("n")) {
